0b2da839ba2c4a7fc534da86ecd43d4c968ffc3b
elizabeth011685 <elizabeth011685@hotmail.com>
2024-06-30 00:37:53 -0600

    Creación de rutas y primera versión de componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Deposit from "./components/deposit";
+import Home from "./components/home";
+import CreateAccount from "./components/create-account";
+import Withdraw from "./components/withdraw";
+import AllData from "./components/all-data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <BrowserRouter>
+          <div className="App">
+              <NavBar/>
+              <br />
+                  <Routes>
+                      <Route path="/" element={<Home />} exact />
+                      <Route path="/create-account" element={<CreateAccount />} exact />
+                      <Route path="/deposit" element={<Deposit />} exact />
+                      <Route path="/withdraw" element={<Withdraw />} exact />
+                      <Route path="/all-data" element={<AllData />} exact />
+                  </Routes>
+          </div>
+      </BrowserRouter>
+      </>
+
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import logo from "../logo.svg";
+
+function Home() {
+    return (
+        <>
+            <h1>Welcome to Bad Bank</h1>
+            <img src={logo} alt="logo"/>
+        </>
+    );
+}
+
+export default Home;
+
