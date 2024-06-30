a74553571ddc499b0df56aecfef981e1929c33b3
elizabeth011685 <elizabeth011685@hotmail.com>
2024-06-30 12:30:28 -0600

    Se agrega funcionalidad para crear otra cuenta una vez que el formulario se envió

--- src/pages/create-account.js
+++ src/pages/create-account.js
@@ -1,5 +1,6 @@
 import {useFormik} from "formik";
 import Card from "../components/card";
+import {useState} from "react";
 const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 let validado = 0;
 
@@ -8,13 +9,16 @@ const cardStyle = {
 };
 function CreateAccount() {
 
+    const [enviado, setEnviado] = useState(false)
+
     const formik = useFormik({
         initialValues: {
+            name: '',
             email: '',
             password: ''
         },
         onSubmit: values => {
-            alert("Success");
+            setEnviado(true);
         },
         validate: values => {
             validado = 1;
@@ -22,10 +26,16 @@ function CreateAccount() {
             if (!values.name) errors.name = "Field required";
             if (!values.email) errors.email = "Field required";
             if (!values.password) errors.password = "Field required";
-            if (values.email && !regex.test(String(values.email).toLowerCase())) errors.email = "Username should be an email";
+            if (values.email && !regex.test(String(values.email).toLowerCase())) errors.email = "Field should be a valid email";
             return errors;
         }
-    })
+    });
+
+    const handleCreateNew = ()=>{
+        setEnviado(false);
+        formik.resetForm();
+    }
+
     return (
         <Card
             title="Create Account"
@@ -33,9 +43,16 @@ function CreateAccount() {
             formik={formik}
             body={
                 (<>
+                    {enviado && (
+                        <div className="alert alert-success" role="alert">
+                            Account created successfully!
+                        </div>
+                    )}
                     <div className="mb-3">
                         <input name="name" id="nameField" type="text" onChange={formik.handleChange}
+                               value={formik.values.name}
                                className="form-control" placeholder="Name" aria-label="Name"
+                               readOnly={enviado}
                                aria-describedby="addon-wrapping"/>
                         {formik.errors.name ?
                             <div style={{color: 'red'}} id="nameError">{formik.errors.name}</div> : null}
@@ -43,6 +60,8 @@ function CreateAccount() {
 
                     <div className="mb-3">
                         <input name="email" id="emailField" type="text" onChange={formik.handleChange}
+                               value={formik.values.email}
+                               readOnly={enviado}
                                className="form-control" placeholder="E-mail" aria-label="EMail"
                                aria-describedby="addon-wrapping"/>
                         {formik.errors.email ?
@@ -51,19 +70,30 @@ function CreateAccount() {
 
                     <div className="mb-3">
                         <input name="password" id="pswField" type="text" onChange={formik.handleChange}
+                               value={formik.values.password}
+                               readOnly={enviado}
                                className="form-control" placeholder="Password" aria-label="Password"
                                aria-describedby="addon-wrapping"/>
-                        {formik.errors.password ?
+                               {formik.errors.password ?
                             <div style={{color: 'red'}} id="pswError">{formik.errors.password}</div> : null}
                     </div>
                 </>)
             }
             footer={
                 (
-                    <button disabled={validado === 0 || Object.keys(formik.errors).length > 0} type="submit"
-                                id="submitBtn" className="btn btn-primary">
-                        Create Account
-                    </button>
+                    <>
+                        {!enviado && (
+                            <button disabled={validado === 0 || Object.keys(formik.errors).length > 0} type="submit"
+                                    id="submitBtn" className="btn btn-primary">
+                                Create Account
+                            </button>
+                        )}
+
+                        {enviado && (
+                            <button className="btn btn-primary" onClick={handleCreateNew}>Create Another Account</button>
+                        )}
+
+                    </>
                 )
             }
         />
