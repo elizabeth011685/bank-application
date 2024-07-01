32b9583fd615a4ef107b9bb87616382d056b7eec
elizabeth011685 <elizabeth011685@hotmail.com>
2024-06-30 21:01:20 -0600

    Corrección de table

--- src/components/table.js
+++ src/components/table.js
@@ -1,33 +1,28 @@
-
 function Table({columns, data}) {
-
     return (
-        <table class="table">
+        <table className="table">
             <thead>
             <tr>
                 <th scope="row">#</th>
                 {
                     columns.map((column, index) => (
-                            <th scope="row">{column}</th>
+                            <th scope="row" key={index}>{column}</th>
                         )
                     )
                 }
-
             </tr>
             </thead>
             <tbody>
             {
                 data.map((row, index) => (
-                    <tr>
+                    <tr key={index}>
                         <td>{index + 1}</td>
                         <td>{row.name}</td>
                         <td>{row.email}</td>
                         <td>{row.password}</td>
-
                     </tr>
                 ))
             }
-
             </tbody>
         </table>
     );
