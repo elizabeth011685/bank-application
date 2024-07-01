60a5eb3a28feb5319544fcfd4ff7330e950654b9
elizabeth011685 <elizabeth011685@hotmail.com>
2024-06-30 20:29:33 -0600

    Corrección de footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,15 @@
-import NavBar from "./navbar";
 
 function Footer() {
 
    return (
 
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div class="col-md-4 d-flex align-items-center">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div className="col-md-4 d-flex align-items-center">
 
-                <span class="mb-3 mb-md-0 text-body-secondary">© 2024</span>
+                <span className="mb-3 mb-md-0 text-body-secondary">© 2024</span>
             </div>
         </footer>
    );
-
 }
 
 export default Footer;
